1623064b8884d6a075bdaf8b12267ed52f7cb498
Ana <analuizadiasdarocha212@gmail.com>
2023-04-25 16:52:42 -0300

    Separação do drawer em um component

--- components/Tab.js
+++ components/Tab.js
@@ -1,86 +1,87 @@
-// import React from 'react';
+import React from 'react';
 
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { BottomNavigation } from 'react-native-paper';
-// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-// import Home from './Screens/Home';
-
-
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomNavigation } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from '../Screens/Home';
+import Perfil from '../Screens/Perfil';
 
 
-// const Tab = createBottomTabNavigator();
 
-// function MyComponent() {
-//   return (
-//     <Tab.Navigator
-//       screenOptions={{
-//         headerShown: false,
-//       }}
-//       tabBar={({ navigation, state, descriptors, insets }) => (
-//         <BottomNavigation.Bar
-//           navigationState={state}
-//          safeAreaInsets={insets}
-//           onTabPress={({ route, preventDefault }) => {
-//             const event = navigation.emit({
-//               type: 'tabPress',
-//               target: route.key,
-//               canPreventDefault: true,
-//             });
 
-//             if (event.defaultPrevented) {
-//               preventDefault();
-//             } else {
-//              navigation.dispatch({
-//                 ...CommonActions.navigate(route.name, route.params),
-//                 target: state.key,
-//               });
-//             }
-//           }}
-//           renderIcon={({ route, focused, color }) => {
-//             const { options } = descriptors[route.key];
-//             if (options.tabBarIcon) {
-//               return options.tabBarIcon({ focused, color, size: 24 });
-//             }
 
-//             return null;
-//           }}
-//           getLabelText={({ route }) => {
-//             const { options } = descriptors[route.key];
-//             const label =
-//               options.tabBarLabel !== undefined
-//                 ? options.tabBarLabel
-//                 : options.title !== undefined
-//                 ? options.title
-//                 : route.title;
+const Tab = createBottomTabNavigator();
 
-//             return label;
-//           }}
-//         />
-//       )}
-//     >
-//       <Tab.Screen
-//         name="Home"
-//         component={Home}
-//         options={{
-//           tabBarLabel: 'Home',
-//           tabBarIcon: ({ color, size }) => {
-//             return <Icon name="home" size={size} color={color} />;
-//           },
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Perfil"
-//         component={Perfil}
-//         options={{
-//           tabBarLabel: 'Perfil',
-//           tabBarIcon: ({ color, size }) => {
-//             return <Icon name="cog" size={size} color={color} />;
-//           },
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// }
-// export default MyComponent;
+function MyComponent() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+         safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+             navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.title;
+
+            return label;
+          }}
+        />
+      )}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="home" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="cog" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+export default MyComponent;
 
